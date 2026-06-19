12918c315c010eec91a9cca2f1c8fea07c7dd108
dhgmrlud00 <dhgmlrud1215@gmail.com>
2026-06-19 17:55:26 +0900

    풋터제작

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Home from './pages/Home';
 import Search from './pages/Search';
-import MovieDetail from './pages/MovieDetail'
+import MovieDetail from './pages/MovieDetail';
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
-      </Routes>  
+      </Routes>
+
+      <Footer/>  
     </BrowserRouter>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        본 사이트는 상업적 목적이 아닌 개인 포트폴리오용으로 제작되었습니다.
+      </p>
+      <p>
+        영화 정보 및 이미지는 TMDB API를 활용하였습니다.
+      </p>
+      <small>© 2026 MOVIE BOX Portfolio Project</small>
+    </footer>
+  );
+}
+
+export default Footer;
